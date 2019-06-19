883af6c627d7d7fdf695c6e8c6e7daf07a4b3b94
Justin Dionisio <justindionisio21@yahoo.com>
2019-06-18 18:48:33 -0700

    First commit. Install/setup webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./app/index.js",
+  devtool: "cheap-module-inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        use: [
+          {
+            loader: "url-loader"
+          }
+        ]
+      }
+    ]
+  },
+  mode: "development",
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./app/index.html"
+    })
+  ]
+};
