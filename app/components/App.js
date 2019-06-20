5248033f8d99d588e2bff1db87958d9a4bfce252
Justin Dionisio <justindionisio21@yahoo.com>
2019-06-19 20:53:32 -0700

    Get data from youtube API

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,38 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      videos: []
+    };
+
+    this.onInputSubmit = this.onInputSubmit.bind(this);
+  }
+
+  onInputSubmit(input) {
+    youtube
+      .get("/search", {
+        params: {
+          q: input
+        }
+      })
+      .then(res => {
+        console.log(res.data.items);
+        this.setState({
+          videos: res.data.items
+        });
+      });
+  }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onInputSubmit={this.onInputSubmit} />I have{" "}
+        {this.state.videos.length} videos.
       </div>
     );
   }
