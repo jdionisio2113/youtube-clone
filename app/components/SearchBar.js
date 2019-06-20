cbee91cc74ace70c08d0a8d1d5e0478a253058ca
Justin Dionisio <justindionisio21@yahoo.com>
2019-06-19 18:00:00 -0700

    Add search bar to UI

---
+++ app/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: ""
+    };
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <input
+            type="text"
+            name="Video"
+            placeholder="Search"
+            autoComplete="off"
+            value={this.state.input}
+            onChange={e => this.setState({ input: e.target.value })}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
